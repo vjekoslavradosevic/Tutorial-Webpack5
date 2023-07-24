4a67a2bfd3f915f5f880430fb34451e92433e86f
Vjekoslav Radošević <vr53601@fer.hr>
2023-07-24 13:58:45 +0200

    formatirano eslintom

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  "ignorePatterns": ["webpack.config.js", "*.txt"], //ignorira datoteku webpack.config.js i sve tekstualne datoteke 
-  rules: {
-    "indent": ["error", 4],
-  },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    ignorePatterns: ['webpack.config.js', '*.txt'], // ignorira datoteku webpack.config.js i sve tekstualne datoteke
+    rules: {
+        indent: ['error', 4],
+    },
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import generateJoke from "./generateJoke";
-import "../styles/sass/main.scss";
-//import "./styles/css/main.css"
-import laughing from "../assets/laughing.svg";
+import generateJoke from './generateJoke';
+import '../styles/sass/main.scss';
+// import "./styles/css/main.css"
+import laughing from '../assets/laughing.svg';
 
-//kod koji za src slike iz template.html postavlja putanju do statickog resursa
-const laughImg = document.getElementById("laughImg");
+// kod koji za src slike iz template.html postavlja putanju do statickog resursa
+const laughImg = document.getElementById('laughImg');
 laughImg.src = laughing;
 
-const jokeBtn = document.getElementById("jokeBtn");
-jokeBtn.addEventListener("click", generateJoke);
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
