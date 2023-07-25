1c3a3d329316627e0e3265d322f5eaa6171992a3
Vjekoslav Radošević <vr53601@fer.hr>
2023-07-25 15:51:04 +0200

    dodan lighthouse modul i njegov CI

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ci: {
+      upload: {
+        target: 'temporary-public-storage', //lokacija spremanja rezultata, rezultate moguce spremiti i na server
+      },
+      assert: {
+        preset: 'lighthouse:no-pwa',
+        assertions: {
+          'csp-xss': 'warn',
+          'categories:performance': ['error', { minScore: 0.85 }],
+          'categories:accessibility': ['error', { minScore: 0.85 }],
+          'categories:best-practices': ['error', { minScore: 0.85 }],
+          'categories:seo': ['error', { minScore: 0.75 }],
+        },
+      },
+    },
+  };
